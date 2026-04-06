d052ad26fd97a6dec34ffb45dc68fcf2e05d67eb
Eugene <eugvok85@gmail.com>
2026-04-06 16:57:18 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const enterArray = String(Math.abs(n)).split('').map(Number).reverse();
+  return `${enterArray.join('')}`;
 };
